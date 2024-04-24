0bac21ca917880106e8dc31fff46bf1e37b34de5
andrenavas <andreynanavas@gmail.com>
2024-04-24 00:36:00 -0500

    ADD: Navbar

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,8 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedGenre, selectFilteredBooks } from '../redux/bookReducer';
 import { getBooks } from "../api/booksAPI";
 import CardBook from "./CardBook";
 
 const Library = () => {
+
+  const dispatch = useDispatch();
+  const filteredBooks = useSelector(selectFilteredBooks);
+  const selectedGenre = useSelector(state => state.books.selectedGenre);
+  const genres = ["Fantasía", "Ciencia ficción", "Zombies", "Terror"];
+  
+  const handleGenreChange = (event) => {
+    const selectedGenre = event.target.value;
+    dispatch(setSelectedGenre(selectedGenre));
+  };
 
   const { isLoading, data: response, isError, error } = useQuery({
     queryKey: ['books'],
@@ -13,11 +25,28 @@ const Library = () => {
   else if (isError) return <div> Error: {error.message}</div>
 
   const library = response?.default?.library ?? [];
-  return (<div>
-    {library.map((book, index) => (
-      <CardBook key={index} book={book.book} />
-    ))}
-  </div>
+  let booksToDisplay = library;
+
+  if (selectedGenre) {
+    booksToDisplay = library.filter(book => book.genre === selectedGenre);
+  } else if (filteredBooks.length) {
+    booksToDisplay = filteredBooks;
+  }
+  // const books = filteredBooks.length ? filteredBooks : library;
+  return (
+    <div>
+    <div>
+    <select value={selectedGenre || ""} onChange={handleGenreChange}>
+        <option value="">Género</option>
+          {genres.map((genre, index) => (
+            <option key={index} value={genre}>{genre}</option>
+          ))}
+        </select>
+        </div>
+      {booksToDisplay.map((book, index) => (
+        <CardBook key={index} book={book.book} />
+      ))}
+    </div>
   )
 }
 
--- src/redux/bookReducer.js
+++ src/redux/bookReducer.js
@@ -1,21 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  
+
   books: [],
+  selectedGenre: null,
 };
 
 const bookSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    
+
     addBook(state, action) {
       state.books.push(action.payload);
+    },
+    setSelectedGenre(state, action) {
+      state.selectedGenre = action.payload;
     },
   },
 });
 
-export const { addBook } = bookSlice.actions;
+export const { addBook, setSelectedGenre } = bookSlice.actions;
 
+export const selectFilteredBooks = (state) => {
+  const selectedGenre = state.books.selectedGenre;
+  if (!selectedGenre) {
+    return state.books.books;
+  }
+  return state.books.books.filter(book => book.genre === selectedGenre);
+};
 export default bookSlice.reducer;
